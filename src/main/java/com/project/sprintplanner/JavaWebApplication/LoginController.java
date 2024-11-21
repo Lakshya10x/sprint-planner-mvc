f651212b6a98a10915d0a91486288364fa74d183
Lakshya10x <lakshyasharma8946@gmail.com>
2024-11-22 01:01:54 +0530

    Basic Logging

--- src/main/java/com/project/sprintplanner/JavaWebApplication/LoginController.java
+++ src/main/java/com/project/sprintplanner/JavaWebApplication/LoginController.java
@@ -1,5 +1,7 @@
 package com.project.sprintplanner.JavaWebApplication;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,11 +17,11 @@ public class LoginController {
      */
     /**
         Model : Putting value which you want to pass from controller to JSP (View)
-        Put the value in ModelMap -> Use the value in JSP using ${AttributeName} (Expression language)
+        Put the value in ModelMap -> Use the value in JSP using ${VariableName} (Expression language)
         ModelMap : Implementation of java.util.Map for use when building model data for use with UI tools.
                     Supports chained calls and generation of model attribute names
      */
-    @RequestMapping("login")
+//    @RequestMapping("login")
     public String goToLoginPage(@RequestParam String name, ModelMap modelmap)
     {
         System.out.println("Request Parameter is : " + name);
@@ -29,4 +31,25 @@ public class LoginController {
         modelmap.put("name"       , name);
         return "LoginPage";
     }
+
+    /**
+        Right imports : org.slf4j.Logger; & org.slf4j.LoggerFactory;
+        Based on the logging level defined in application.properties, all the things below that level will be printed.
+        However, Logger -> Able to control the level.
+        Don't want to use debug information -> Change the logging level
+     */
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    @RequestMapping("login")
+    public String openLoginPage(@RequestParam String name, ModelMap modelmap)
+    {
+        modelmap.put("name", name);
+        logger.info("Printed at INFO level");
+        logger.warn("Printed at WARN level");
+        logger.debug("Request Param is {}",name);
+
+        // By SOP, you cannot configure the level's or anything like that
+        return "LoginPage";
+    }
+
 }
